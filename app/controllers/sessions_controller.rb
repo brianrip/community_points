6074076643964730c438bb10f3f3e8132be375c7
Brian Rippeto <rippeto@gmail.com>
2016-02-26 17:53:16 -0700

    Add helper methods for current user and admin.
    * Refactor create session action to use helper methods for current user.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,35 +9,10 @@ class SessionsController < ApplicationController
 
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      if user.role == 1
-        redirect_to admin_index_path
-      else
-        redirect_to user_path(user)
-      end
+      redirect_to user.authorize_session
     else
       flash[:errors] = "Invalid Login"
       render :new
     end
   end
 end
-
-
-
-
-
-# def create
-#   user = User.find_by(username:params[:session][:username])
-#   if user && user.authenticate(params[:session][:password])
-#     session[:user_id] = user.id
-#     redirect_to user_path(user)
-#   else
-#     flash[:errors] = "Invalid Login"
-#     render :new
-#   end
-# end
-#
-# def destroy
-#   session.clear
-#   redirect_to root_path
-# end
-# end
